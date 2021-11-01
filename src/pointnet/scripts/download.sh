8e608bc4dedf3e72ab2b5849f8134c6da74c43ad
Matthew Kelleher <kelleher@cs.utexas.edu>
2021-10-31 20:13:28 -0500

    add script to prepare data for modelnet

--- src/pointnet/scripts/download.sh
+++ src/pointnet/scripts/download.sh
@@ -5,4 +5,13 @@ cd $SCRIPTPATH/..
 wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
 unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
 rm shapenetcore_partanno_segmentation_benchmark_v0.zip
+
+wget http://3dvision.princeton.edu/projects/2014/3DShapeNets/ModelNet10.zip --no-check-certificate
+unzip ModelNet10.zip
+rm ModelNet10.zip
+
+wget http://modelnet.cs.princeton.edu/ModelNet40.zip --no-check-certificate
+unzip ModelNet40.zip
+rm ModelNet40.zip
+
 cd -
